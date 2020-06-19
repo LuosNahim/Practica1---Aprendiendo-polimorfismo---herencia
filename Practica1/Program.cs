bcaefafb468df00c3b255057ef2155c8786d8be1
riusu2 <antonio.nava@processmaker.com>
2020-06-18 22:24:19 -0400

    Agregar archivos de proyecto.

---
+++ Practica1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Practica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Avion test = new Avion();
+            test.Arrancar();
+            test.Conducir();
+        }
+    }
+
+    class Vehiculo
+    {
+        public void Arrancar()
+        {
+            Console.WriteLine( "He arrancado el motor");
+        }
+        public void Parar()
+        {
+            Console.WriteLine("El motor se ha detenido");
+        }
+        public virtual void Conducir()
+        {
+            Console.WriteLine("conducir el vehiculo");
+        }
+
+    }
+
+    class Avion :Vehiculo
+    {
+        public void Volar()
+        {
+            Console.WriteLine("Volando");
+        }
+        public override void Conducir()
+        {
+            Console.WriteLine("COnduciendo un AVION");
+        }
+    }
+    class Coche : Vehiculo
+    {
+         public void Moverse()
+        {
+        Console.WriteLine("cicular en las calles");
+        }
+        public override void Conducir()
+        {
+            Console.WriteLine("COnduce un coche");
+        }
+    }
+
+
+
+
+}
